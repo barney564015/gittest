ac3931153072db4728e0c4e8afa52c11d79a29b2
bgleadall <bgleadall@bgleadall-ThinkPad-T430>
2014-12-16 06:00:41 -0500

    testing the createMethod branch

--- gittest/src/com/gittest/GitTest.java
+++ gittest/src/com/gittest/GitTest.java
@@ -3,7 +3,11 @@ package com.gittest;
 public class GitTest {
 
 	public static void main(String[] args) {
+		testMethod();
 		System.out.println("This is our test");
 	}
 
+	public static void testMethod(){
+		System.out.println("this is another branch we are testing");
+	}
 }
