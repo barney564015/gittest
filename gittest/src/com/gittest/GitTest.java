557ff05a0bc518c7e7d751d25d83b0e4f35f8fe7
bgleadall <bgleadall@bgleadall-ThinkPad-T430>
2014-12-16 06:03:38 -0500

    added another method to master

--- gittest/src/com/gittest/GitTest.java
+++ gittest/src/com/gittest/GitTest.java
@@ -4,6 +4,10 @@ public class GitTest {
 
 	public static void main(String[] args) {
 		System.out.println("This is our test");
+		anotherMethod();
 	}
 
+	public static void anotherMethod(){
+		System.out.println("we have the another method here");
+	}
 }
